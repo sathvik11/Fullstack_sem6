1c2d14ad93e2401f49c69e5b43c70e21525915ea
sathvik11 <94104706+sathvik11@users.noreply.github.com>
2023-03-19 10:01:36 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,5 +6,13 @@ import (
 )
 
 func main() {
-	fmt.Println("My favorite number is", rand.Intn(100))
+
+	num1 := rand.Intn(10) + 1
+	num2 := rand.Intn(10) + 1
+
+	// Add the two variables together and store the result in a third variable
+	sum := num1 + num2
+
+	// Print out the values of the variables and the sum of the two numbers
+	fmt.Printf("num1: %d\nnum2: %d\nsum: %d\n", num1, num2, sum)
 }
