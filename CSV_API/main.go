a1865556927b99b95354a74a8ab06a1d9681e3a2
sathvik11 <94104706+sathvik11@users.noreply.github.com>
2023-03-30 20:46:13 +0530

    Update main.go

--- CSV_API/main.go
+++ CSV_API/main.go
@@ -3,46 +3,40 @@ package main
 import (
 	"encoding/csv"
 	"log"
-	"fmt"
 	"net/http"
 	"os"
 	"strconv"
 
-	"github.com/gin-gonic/gin"
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 type Data struct {
-	ID    int    `csv:"id"`
-	name  string `csv:"name"`
-	age  string  `csv:"age"`
-	loc  string `csv:"location"`
+	ID       int    `csv:"id"`
+	Name     string `csv:"name"`
+	Age      string `csv:"age"`
+	Location string `csv:"location"`
 }
 
 func main() {
 	router := gin.Default()
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"*"},
-		AllowHeaders:     []string{"*"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-	}))
+	router.Use(cors.Default())
 
-	router.GET("/data", getData)
-	router.GET("/data", getOneData)
-	router.POST("/data", createData)
-	router.PUT("/data/:id", updateData)
-	router.DELETE("/data/:id", deleteData)
+	router.GET("/data", fileExists(), getData)
+	router.GET("/data/:id", fileExists(), getOneData)
+	router.POST("/data", fileExists(), createData)
+	router.PUT("/data/:id", fileExists(), updateData)
+	router.DELETE("/data/:id", fileExists(), deleteData)
 
-	router.Run("localhost:5700")
+	if err := router.Run(":5700"); err != nil {
+		log.Fatal(err)
+	}
 }
 
-//Checking if file exists
+// Check if the CSV file exists
 func fileExists() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		_, err := os.Stat("user.csv")
-		if os.IsNotExist(err) {
+		if _, err := os.Stat("user.csv"); os.IsNotExist(err) {
 			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "CSV file not found"})
 			return
 		}
@@ -50,9 +44,7 @@ func fileExists() gin.HandlerFunc {
 	}
 }
 
-
-//Getting data from CSV file using GET method
-
+// Get data from the CSV file using GET method
 func getData(c *gin.Context) {
 	file, err := os.Open("user.csv")
 	if err != nil {
@@ -68,10 +60,8 @@ func getData(c *gin.Context) {
 		return
 	}
 
-
 	var result []Data
 	for i, row := range records {
-		// Skipping the header row as it would be column names
 		if i == 0 {
 			continue
 		}
@@ -82,24 +72,28 @@ func getData(c *gin.Context) {
 			return
 		}
 
-		// Creating a new Data object
 		data := Data{
-			ID:    id,
-			Name:  row[1],
-			Age: row[2],
+			ID:       id,
+			Name:     row[1],
+			Age:      row[2],
 			Location: row[3],
 		}
 
 		result = append(result, data)
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Getting data, Successfu!", "Data": result})
+	c.JSON(http.StatusOK, gin.H{"message": "Getting data successful!", "data": result})
 }
 
-
-//Getting filtered data from CSV file using GET method
-
+// Get filtered data from the CSV file using GET method
 func getOneData(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid ID parameter"})
+		return
+	}
+
 	file, err := os.Open("user.csv")
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -114,42 +108,87 @@ func getOneData(c *gin.Context) {
 		return
 	}
 
-	params := c.Request.URL.Query()
-
-	var filteredRecords [][]string
-	for _, row := range records {
-		if row[0] == "id" {
+	var result Data
+	for i, row := range records {
+		if i == 0 {
 			continue
 		}
 
-		match := true
-		for column, value := range params {
-			if column >= len(row) {
-				continue
-			}
-
-			if row[column] != value[0] {
-				match = false
-				break
-			}
+		recordID, err := strconv.Atoi(row[0])
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
 
-		if match {
-			filteredRecords = append(filteredRecords, row)
+		if recordID == id {
+			result = Data{
+				ID:       recordID,
+				Name:     row[1],
+				Age:      row[2],
+				Location: row[3],
+			}
 		}
+		// Get all data from the CSV file using GET method
+func getAllData(c *gin.Context) {
+	file, err := os.Open("user.csv")
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Records filtered successfully!", "data": filteredRecords})
+	var result []Data
+	for i, row := range records {
+		if i == 0 {
+			continue
+		}
+
+		id, _ := strconv.Atoi(row[0])
+		age, _ := strconv.Atoi(row[2])
+
+		result = append(result, Data{
+			ID:       id,
+			Name:     row[1],
+			Age:      age,
+			Location: row[3],
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Getting data successful!", "data": result})
 }
 
-
-//create new data in the CSV file using POST method
-
+// Create new data in the CSV file using POST method
 func createData(c *gin.Context) {
 	var data Data
 	if err := c.ShouldBindJSON(&data); err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
+	}
+
+	// Check if CSV file exists
+	if _, err := os.Stat("user.csv"); os.IsNotExist(err) {
+		file, err := os.Create("user.csv")
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		defer file.Close()
+
+		writer := csv.NewWriter(file)
+		defer writer.Flush()
+
+		header := []string{"id", "name", "age", "location"}
+		if err := writer.Write(header); err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
 	}
 
 	file, err := os.OpenFile("user.csv", os.O_APPEND|os.O_WRONLY, 0644)
@@ -162,161 +201,73 @@ func createData(c *gin.Context) {
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	err = writer.Write([]string{
+	record := []string{
 		strconv.Itoa(data.ID),
 		data.Name,
-		data.age,
-		data.loc,
-	})
-	if err != nil {
+		strconv.Itoa(data.Age),
+		data.Location,
+	}
+
+	if err := writer.Write(record); err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Data created successfully","Data": map[string]interface{}{"data": result}})
+	c.JSON(http.StatusOK, gin.H{"message": "Data created successfully!", "data": data})
 }
 
-
-//updating data in the CSV file using PUT method
-
+// Update data in the CSV file using PUT method
 func updateData(c *gin.Context) {
-idParam := c.Param("id")
-id, err := strconv.Atoi(idParam)
-if err != nil {
-	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid ID parameter"})
-	return
-}
-
-var data Data
-if err := c.ShouldBindJSON(&data); err != nil {
-	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	return
-}
-
-file, err := os.Open("user.csv")
-if err != nil {
-	c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	return
-}
-defer file.Close()
-
-reader := csv.NewReader(file)
-records, err := reader.ReadAll()
-if err != nil {
-	c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	return
-}
-
-var found bool
-for i, row := range records {
-	// Skipping the header row as it is column names
-	if i == 0 {
-		continue
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid ID parameter"})
+		return
 	}
 
-	rowID, err := strconv.Atoi(row[0])
+	var data Data
+	if err := c.ShouldBindJSON(&data); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	file, err := os.OpenFile("user.csv", os.O_RDWR, 0644)
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	defer file.Close()
 
-	if rowID == id {
-		records[i][1] = data.Name
-		records[i][2] = data.Age
-		records[i][3] = data.Location
-		found = true
-		break
-	}
-}
+	reader := csv.NewReader(file)
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
 
-if !found {
-	c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "Record not found"})
-	return
-}
-
-file, err = os.OpenFile("user.csv", os.O_WRONLY|os.O_TRUNC, 0644)
-if err != nil {
-	c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	return
-}
-defer file.Close()
-
-writer := csv.NewWriter(file)
-defer writer.Flush()
-
-err = writer.WriteAll(records)
-if err != nil {
-	c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	return
-}
-
-c.JSON(http.StatusOK, gin.H{"message": "Records updated successfully"})
-}
-
-
-//deleting data from the CSV file using DELETE Method
-
-func deleteData(c *gin.Context) {
-idParam := c.Param("id")
-
-id, err := strconv.Atoi(idParam)
-if err != nil {
-	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid ID parameter"})
-	return
-}
-
-file, err := os.Open("user.csv")
-if err != nil {
-	c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error})
-defer file.Close()
-
-reader := csv.NewReader(file)
-records, err := reader.ReadAll()
-if err != nil {
-	c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	return
-}
-
-var found bool
-for i, row := range records {
-	// since the header row is column names
-	if i == 0 {
-		continue
-	}
-
-	rowID, err := strconv.Atoi(row[0])
+	records, err := reader.ReadAll()
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
+		}
+		var updatedRecords [][]string
+for _, record := range records {
+	if record[0] == strconv.Itoa(id) {
+		record[1] = data.Name
+		record[2] = data.Age
+		record[3] = data.Email
+		record[4] = data.Address
+		record[5] = data.PhoneNumber
 	}
+	updatedRecords = append(updatedRecords, record)
+}
 
-	if rowID == id {
-		records = append(records[:i], records[i+1:]...)
-		found = true
-		break
+file.Truncate(0)
+file.Seek(0, 0)
+
+for _, updatedRecord := range updatedRecords {
+	if err := writer.Write(updatedRecord); err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
 	}
 }
 
-if !found {
-	c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "Record not found"})
-	return
-}
-
-file, err = os.OpenFile("data.csv", os.O_WRONLY|os.O_TRUNC, 0644)
-if err != nil {
-	c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	return
-}
-defer file.Close()
-
-writer := csv.NewWriter(file)
-defer writer.Flush()
-
-err = writer.WriteAll(records)
-if err != nil {
-	c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	return
-}
-
-c.JSON(http.StatusOK, gin.H{"message": "Record deleted successfully"})
+c.JSON(http.StatusOK, gin.H{"message": "Data updated successfully"})
 }
