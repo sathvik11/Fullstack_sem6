1c8d5b3486ac6d2eae31f910a11feef7ab9da117
sathvik11 <94104706+sathvik11@users.noreply.github.com>
2023-03-30 16:57:24 +0530

    Update Selectors.go

--- Internal Assignments/MongoDB_selectors/Selectors.go
+++ Internal Assignments/MongoDB_selectors/Selectors.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -15,37 +14,20 @@ import (
 // Connection URI
 const uri = "mongodb://nikx-training-mongo:4ZR3wvYKDbgix7TRvJ8EsaBJMgQKGL165f1m1vLB1hRSDNIEhMGSAHobpHaPbca5IXKHGi5zG7kkACDbnNrowA==@nikx-training-mongo.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@nikx-training-mongo@"
 
-var client, _ = mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+var client, _ = mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
 var coll = client.Database("mflix").Collection("movies")
 
 func main() {
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"*"},
-		AllowHeaders:     []string{"*"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:    []string{"Origin", "Content-Type", "Content-Length"},
 	}))
 
-	router.GET("/movie", lessThan)
-	router.GET("/movie", lessThaneqto)
-	router.GET("/movie", equalTo)
-	router.GET("/movie", greaterThan)
-	router.GET("/movie", greaterThaneqto)
-	router.GET("/movie", andSelec)
-	router.GET("/movie", notSelec)
+	router.GET("/movies", searchMovies)
 
 	router.Run("localhost:5700")
-}
-
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Headers", "*")
-		c.Header("Access-Control-Allow-Methods", "*")
-		c.Next()
-	}
 }
 
 type Movie struct {
@@ -54,205 +36,47 @@ type Movie struct {
 	Rated string `bson:"rated,omitempty"`
 }
 
-// Displaying movies with rating less than 3
-func lessThan(c *gin.Context) {
-	size, _ := strconv.ParseInt(c.Query("size"), 10, 64)
-	title := c.Query("title")
-	opts := options.Find().SetLimit(size)
-	filter := bson.M{"Rated": bson.M{"$lt": 3}}
-	if title != "" {
-		filter = bson.M{"title": title}
-	}
-	cursor, err := coll.Find(context.TODO(), filter, opts)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			// This error means your query did not match any documents.
-			c.JSON(http.StatusNoContent, gin.H{"message": "No Content"})
-			return
-		}
-		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
-		return
-	}
-
-	var results []Movie
-	if err = cursor.All(context.TODO(), &results); err != nil {
-		panic(err)
-	}
-
-	res := map[string]interface{}{"data": results}
-	c.JSON(http.StatusOK, gin.H{"message": "Displaying data  with less than 3 ratings!", "Data": res})
+type SearchFilter struct {
+	Title  string `json:"title,omitempty"`
+	Rating int    `json:"rating,omitempty"`
 }
 
-// Displaying movies with rating less than or equal to 5
-func lessThaneqto(c *gin.Context) {
-	size, _ := strconv.ParseInt(c.Query("size"), 10, 64)
-	title := c.Query("title")
-	opts := options.Find().SetLimit(size)
-	filter := bson.M{"Rated": bson.M{"$lte": 5}}
-	if title != "" {
-		filter = bson.M{"title": title}
+func searchMovies(c *gin.Context) {
+	var searchFilter SearchFilter
+	if err := c.ShouldBindQuery(&searchFilter); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
-	cursor, err := coll.Find(context.TODO(), filter, opts)
+
+	filter := bson.M{}
+	if searchFilter.Title != "" {
+		filter["title"] = searchFilter.Title
+	}
+	if searchFilter.Rating != 0 {
+		filter["rated"] = bson.M{"$eq": searchFilter.Rating}
+	}
+
+	opts := options.Find().SetLimit(50)
+	cursor, err := coll.Find(context.Background(), filter, opts)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
-			// This error means your query did not match any documents.
-			c.JSON(http.StatusNoContent, gin.H{"message": "No Content"})
+			c.JSON(http.StatusNotFound, gin.H{"error": "No documents found"})
 			return
 		}
-		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
 	var results []Movie
-	if err = cursor.All(context.TODO(), &results); err != nil {
-		panic(err)
-	}
-
-	res := map[string]interface{}{"data": results}
-	c.JSON(http.StatusOK, gin.H{"message": "Displaying data with less than and equal to 5 ratings !", "Data": res})
-}
-
-// Displaying movies with rating equal to 10
-func equalTo(c *gin.Context) {
-	size, _ := strconv.ParseInt(c.Query("size"), 10, 64)
-	title := c.Query("title")
-	opts := options.Find().SetLimit(size)
-	filter := bson.M{"Rated": bson.M{"$eq": 10}}
-	if title != "" {
-		filter = bson.M{"title": title}
-	}
-	cursor, err := coll.Find(context.TODO(), filter, opts)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			// This error means your query did not match any documents.
-			c.JSON(http.StatusNoContent, gin.H{"message": "No Content"})
-			return
-		}
-		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
+	if err := cursor.All(context.Background(), &results); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	var results []Movie
-	if err = cursor.All(context.TODO(), &results); err != nil {
-		panic(err)
-	}
-
-	res := map[string]interface{}{"data": results}
-	c.JSON(http.StatusOK, gin.H{"message": "Displaying data with 10 ratings !", "Data": res})
-}
-
-// Displaying movies with rating greater than 5
-func greaterThan(c *gin.Context) {
-	size, _ := strconv.ParseInt(c.Query("size"), 10, 64)
-	title := c.Query("title")
-	opts := options.Find().SetLimit(size)
-	filter := bson.M{"Rated": bson.M{"$gt": 5}}
-	if title != "" {
-		filter = bson.M{"title": title}
-	}
-	cursor, err := coll.Find(context.TODO(), filter, opts)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			// This error means your query did not match any documents.
-			c.JSON(http.StatusNoContent, gin.H{"message": "No Content"})
-			return
-		}
-		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
+	if len(results) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "No documents found"})
 		return
 	}
 
-	var results []Movie
-	if err = cursor.All(context.TODO(), &results); err != nil {
-		panic(err)
-	}
-
-	res := map[string]interface{}{"data": results}
-	c.JSON(http.StatusOK, gin.H{"message": "Displaying data with greater than 5 ratings !", "Data": res})
-}
-
-// Displaying movies with rating greater than or equal to 7
-func greaterThaneqto(c *gin.Context) {
-	size, _ := strconv.ParseInt(c.Query("size"), 10, 64)
-	title := c.Query("title")
-	opts := options.Find().SetLimit(size)
-	filter := bson.M{"Rated": bson.M{"$gte": 7}}
-	if title != "" {
-		filter = bson.M{"title": title}
-	}
-	cursor, err := coll.Find(context.TODO(), filter, opts)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			// This error means your query did not match any documents.
-			c.JSON(http.StatusNoContent, gin.H{"message": "No Content"})
-			return
-		}
-		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
-		return
-	}
-
-	var results []Movie
-	if err = cursor.All(context.TODO(), &results); err != nil {
-		panic(err)
-	}
-
-	res := map[string]interface{}{"data": results}
-	c.JSON(http.StatusOK, gin.H{"message": "Displaying data with greater than and equal to 7 rating !", "Data": res})
-}
-
-// Displaying movies with  title "Minions" and a rating greater than 5
-func andSelec(c *gin.Context) {
-	size, _ := strconv.ParseInt(c.Query("size"), 10, 64)
-	title := c.Query("title")
-	opts := options.Find().SetLimit(size)
-	filter := bson.M{"$and": bson.A{bson.M{"Title": "Minions"}, bson.M{"Rated": bson.M{"$gt": 5}}}}
-	if title != "" {
-		filter = bson.M{"title": title}
-	}
-	cursor, err := coll.Find(context.TODO(), filter, opts)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			// This error means your query did not match any documents.
-			c.JSON(http.StatusNoContent, gin.H{"message": "No Content"})
-			return
-		}
-		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
-		return
-	}
-
-	var results []Movie
-	if err = cursor.All(context.TODO(), &results); err != nil {
-		panic(err)
-	}
-
-	res := map[string]interface{}{"data": results}
-	c.JSON(http.StatusOK, gin.H{"message": "Displaying data with title of the movie as 'minions' and with ratings greater than 5!", "Data": res})
-}
-
-// Displaying movies which do NOT have the title "Sabahat"
-func notSelec(c *gin.Context) {
-	size, _ := strconv.ParseInt(c.Query("size"), 10, 64)
-	title := c.Query("title")
-	opts := options.Find().SetLimit(size)
-	filter := bson.M{"$not": bson.A{bson.M{"Title": "Sabahat"}}}
-	if title != "" {
-		filter = bson.M{"title": title}
-	}
-	cursor, err := coll.Find(context.TODO(), filter, opts)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			// This error means your query did not match any documents.
-			c.JSON(http.StatusNoContent, gin.H{"message": "No Content"})
-			return
-		}
-		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
-		return
-	}
-
-	var results []Movie
-	if err = cursor.All(context.TODO(), &results); err != nil {
-		panic(err)
-	}
-
-	res := map[string]interface{}{"data": results}
-	c.JSON(http.StatusOK, gin.H{"message": "Displaying data which do not have the title 'Sabahat'!", "Data": res})
+	c.JSON(http.StatusOK, gin.H{"data": results})
 }
